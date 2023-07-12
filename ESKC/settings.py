9239add7265160cd37cbc6fc0670a9a792f5c451
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-07-12 18:29:51 +0300

    Update settings.py

--- ESKC/settings.py
+++ ESKC/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['eskcadmin.onrender.com',  '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://www.eskc.org.et/
+    'http://www.eskc.org.et/'
 ]
 
 # Application definition
